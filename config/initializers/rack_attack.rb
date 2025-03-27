c188bc08094d4f90c153c084564964a2b7191ee8
michael021212 <michael021212@gmail.com>
2025-03-27 08:04:17 +0000

    rack_attack.rbの修正

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,21 +1,22 @@
 class Rack::Attack
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
 
-  throttle("req/ip", limit: 3, period: 1.minute) do |req|
-    req.ip
+  # リクエストの制限を設定
+  Rack::Attack.throttle('req/ip', limit: 300, period: 5.minutes) do |req|
+    req.ip unless req.path.start_with?('/webhook')
   end
 
-  throttle("webhook/ip", limit: 1, period: 30.seconds) do |req|
-    if req.path == "/webhook"
-      req.ip
-    end
+  # LINE Webhookの制限を設定
+  Rack::Attack.throttle('webhook/ip', limit: 100, period: 1.minute) do |req|
+    req.ip if req.path.start_with?('/webhook')
   end
 
+  # ブロックされたリクエストのレスポンス
   Rack::Attack.throttled_responder = lambda do |env|
     [
-      429,
-      { "Content-Type" => "text/plain" },
-      [ "Too many requests. Please try again later." ]
+      429, # status
+      { 'Content-Type' => 'application/json' },
+      [{ error: 'Too many requests' }.to_json]
     ]
   end
 end
